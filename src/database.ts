eaf6a1eecffd4a5dbee2665ca24bf5f3fe14a11a
vixhnuchandran <vixhnuchandran@gmail.com>
2024-01-03 02:41:29 +0530

    feat: Set up PostgreSQL connection pool in database.ts

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,9 @@
 import { Pool, PoolClient } from "pg"
-import * as dotenv from "dotenv"
+import dotenv from "dotenv"
 
 dotenv.config()
 
-const connectionString: string =
-  "postgres://default:yM8quNa7gGBH@ep-dry-glitter-33364785.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require"
+const connectionString: string = process.env.POSTGRES_URL!
 
 if (!connectionString) {
   console.error("POSTGRES_URL is not defined in the environment variables.")
