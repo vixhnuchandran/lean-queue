294bae590a2c835cbbf688b4cc0c66f0ca6fa240
vixhnuchandran <vixhnuchandran@gmail.com>
2024-01-08 03:50:24 +0530

    refactor: Simplify logging in server.ts
    In server.ts, simplify the logging by using the unified logger from utils/logger. Removed the usage of the dedicated wLogger for this scenario, streamlining the code and ensuring consistency in logging throughout the application.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app"
-import { logger, wLogger } from "./utils/logger"
+import { logger } from "./utils/logger"
 
 const PORT: number = 8484
 app.listen(PORT, () => {
-  wLogger.info(`Server listening on port ${PORT}`)
+  logger.info(`Server listening on port ${PORT}`)
 })
