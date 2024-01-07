d33dd524e250ddc333ffb773435f0f0da666a22d
vixhnuchandran <vixhnuchandran@gmail.com>
2024-01-08 03:51:31 +0530

    refactor: Improve error handling and logging in error.ts
    In error.ts, refactor error classes to include error codes for better identification of errors. Enhanced logging by using structured logging with requestId for better traceability. Updated the error handling middleware to reflect the changes in the error classes and to provide more detailed logs.

--- src/error.ts
+++ src/error.ts
@@ -1,25 +1,56 @@
 import { Request, Response, NextFunction } from "express"
-import { logger, wLogger } from "./utils/logger"
+import { logger } from "./utils/logger"
 import {
   StatusCodes as httpCodes,
   ReasonPhrases as phrases,
 } from "http-status-codes"
 
+export const ValidationErrorCode = {
+  EMPTY_REQUEST_BODY: "VAL-001",
+  INVALID_TYPE: "VAL-002",
+  INVALID_TYPE_FORMAT: "VAL-003",
+  INVALID_QUEUE_ID: "VAL-004",
+  QUEUE_NOT_EXIST: "VAL-005",
+  INVALID_OPTIONS_OBJECT: "VAL-006",
+  INVALID_CALLBACK_TYPE: "VAL-007",
+  INVALID_CALLBACK_FORMAT: "VAL-008",
+  INVALID_EXPIRY_TIME: "VAL-009",
+  EMPTY_TASKS: "VAL-010",
+  TASK_NOT_AN_OBJECT: "VAL-011",
+  TASK_EMPTY_OBJECT: "VAL-012",
+  TASKS_EMPTY_OBJECT: "VAL-013",
+  TASK_ID_MISSING: "VAL-014",
+  PARAMS_NOT_AN_OBJECT: "VAL-015",
+  OPTIONS_NOT_AN_OBJECT: "VAL-016",
+  EMPTY_PARAMS: "VAL-017",
+  PRIORITY_NOT_AN_INTEGER: "VAL-018",
+  INVALID_PRIORITY: "VAL-019",
+  MISSING_PROPERTY: "VAL-020",
+  MISSING_QUEUE_ID: "VAL-021",
+}
+export const QueueErrorCode = {
+  QUEUE_NOT_EXIST: "QUE-001",
+}
+
 // Custom error class for errors related to queue operations
 export class QueueError extends Error {
-  constructor(message: string) {
+  errorCode: string
+  constructor(message: string, errorCode: string) {
     super(message)
     this.name = "QueueError"
     this.message = message
+    this.errorCode = errorCode
   }
 }
 
 // Custom error class for errors related to validation errors
 export class ValidationError extends Error {
-  constructor(message: string) {
+  errorCode: string
+  constructor(message: string, errorCode: string) {
     super(message)
     this.name = "ValidationError"
     this.message = message
+    this.errorCode = errorCode
   }
 }
 
@@ -29,13 +60,18 @@ const handleCustomError = (
   req: Request,
   res: Response
 ): Response => {
+  const requestId = req.requestId
+
   if (err instanceof ValidationError || err instanceof QueueError) {
-    logger.error(`${err.constructor.name}: ${err.message}`)
+    logger.error({
+      message: `${err.constructor.name}: ${err.message}`,
+      requestId,
+    })
 
     return res.status(httpCodes.BAD_REQUEST).json({ error: err.message })
   }
 
-  logger.error(`error: ${err.message}`)
+  logger.error({ message: `error: ${err.message}`, requestId })
 
   return res
     .status(httpCodes.INTERNAL_SERVER_ERROR)
@@ -44,7 +80,9 @@ const handleCustomError = (
 
 // Handles unknown errors
 const handleUnknownError = (err: Error, req: Request, res: Response) => {
-  logger.error(`error: ${err.message}`)
+  const requestId = req.requestId
+
+  logger.error({ message: `error: ${err.message}`, requestId })
 
   return res
     .status(httpCodes.INTERNAL_SERVER_ERROR)
