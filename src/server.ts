853285c0a4bdc0236205e63130f211e239655d19
vixhnuchandran <vixhnuchandran@gmail.com>
2024-02-15 13:35:14 +0530

    chore: add certificate from  zerossl

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,27 @@
 import app from "./app"
-import { logger } from "./utils/logger"
+// import { logger } from "./utils/logger"
+import fs from "fs"
+import https from "https"
+import path from "path"
+try {
+  const keyPath = path.resolve(__dirname, "private.key")
+  const key = fs.readFileSync(keyPath)
 
-const PORT: number = 8484
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
+  const certPath = path.resolve(__dirname, "certificate.crt")
+  const cert = fs.readFileSync(certPath)
+
+  const cred = {
+    key,
+    cert,
+  }
+
+  const httpsServer = https.createServer(cred, app)
+
+  const PORT = 8484
+
+  httpsServer.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+  })
+} catch (error) {
+  console.error("Error reading certificate files:", error)
+}
