8f791d14dbdc238934248b1f3e7482a4a505d1e4
vixhnuchandran <vixhnuchandran@gmail.com>
2024-02-29 15:17:56 +0530

    chore: comment out http server settings

--- src/server.ts
+++ src/server.ts
@@ -4,30 +4,25 @@ import fs from "fs"
 import https from "https"
 import path from "path"
 try {
-  const keyPath = path.resolve(__dirname, "../certificates", "private.key")
-  const key = fs.readFileSync(keyPath)
+    // const keyPath = path.resolve(__dirname, "../certificates", "private.key")
+    // const key = fs.readFileSync(keyPath)
 
-  const certPath = path.resolve(__dirname, "../certificates", "certificate.crt")
-  const cert = fs.readFileSync(certPath)
+    // const certPath = path.resolve(__dirname, "../certificates", "certificate.crt")
+    // const cert = fs.readFileSync(certPath)
 
-  const cred = {
-    key,
-    cert,
-  }
+    // const cred = {
+    //   key,
+    //   cert,
+    // }
 
-  const httpsServer = https.createServer(cred, app)
+    // const httpsServer = https.createServer(cred, app)
 
-  const PORT = 8484
+    const PORT = 8484
 
-  // HTTPS SERVER
-  // httpsServer.listen(PORT, () => {
-  //   console.log(`Server listening on port ${PORT}`)
-  // })
-  
-  // HTTP SERVER
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-  })
+    // HTTP SERVER
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`)
+    })
 } catch (error) {
-  console.error("Error reading certificate files:", error)
+    console.error("Error reading certificate files:", error)
 }
